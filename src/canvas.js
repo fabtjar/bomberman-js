f13bf534252e20a35f53e63ac5fb91d72ed741fd
Fabian Jarrett <fabianjarrett@gmail.com>
2020-01-06 18:59:08 +0000

    Add draw method

--- src/canvas.js
+++ src/canvas.js
@@ -11,19 +11,23 @@ export class Canvas {
             "image-rendering: pixelated;"
         );
 
-        let ctx = canvas.getContext('2d');
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, width, height);
+        this.ctx = canvas.getContext('2d');
+        this.ctx.fillStyle = "white";
+        this.ctx.fillRect(0, 0, width, height);
 
         let image = new Image();
         image.src = "../assets/sprites/wall.png";
         image.onload = () => {
-            ctx.drawImage(image, 0, 0);
-            ctx.drawImage(image, 16, 0);
-            ctx.drawImage(image, 16, 16);
-            ctx.drawImage(image, 32, 16);
-            ctx.drawImage(image, 48, 16);
-            ctx.drawImage(image, 32, 32);
+            this.draw(image, 0, 0);
+            this.draw(image, 16, 0);
+            this.draw(image, 16, 16);
+            this.draw(image, 32, 16);
+            this.draw(image, 48, 16);
+            this.draw(image, 32, 32);
         }
+    }
+
+    draw(image, x, y) {
+        this.ctx.drawImage(image, x, y);
     }
 }
