45efa6ee63289a7d233ec1b5d78da25536ee5abd
Fabian Jarrett <fabianjarrett@gmail.com>
2020-01-06 20:40:33 +0000

    Add update loop

--- src/game.js
+++ src/game.js
@@ -4,14 +4,40 @@ import { GameObject } from "./GameObject.js";
 
 export class Game {
     constructor() {
-        let canvas = new Canvas(150, 100);
-        
+        this.canvas = new Canvas(150, 100);
+
         let image = new Image();
         image.src = "../assets/sprites/wall.png";
         image.onload = () => {
             let sprite = new Sprite(image);
-            let box = new GameObject(sprite, 32, 32);
-            box.draw(canvas);
+            this.box = new GameObject(sprite, 32, 32);
+
+            this.lastTime = this.getTime();
+            this.update();
         }
+    }
+
+    update() {
+        let dt = this.getDeltaTime();
+        this.box.update(dt);
+        this.canvas.clear();
+        this.box.draw(this.canvas);
+        requestAnimationFrame(() => this.update());
+    }
+
+    getDeltaTime() {
+        let now = this.getTime();
+        let dt = (now - this.lastTime) / 1000;
+        this.lastTime = now;
+
+        // Incase loosing focus causing a huge delay
+        return Math.min(1, dt);
+    }
+
+    getTime() {
+        if (window.performance && window.performance.now) {
+            return window.performance.now();
+        }
+        return new Date().getTime();
     }
 }
